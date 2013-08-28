7008fa0ad4e625874549410bd0af428475726ca2
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-08-28 19:34:03 +0000

    - Prepped for test build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'home_reports';
-$app['version'] = '1.4.35';
+$app['version'] = '1.5.0';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
