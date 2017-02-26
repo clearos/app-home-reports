d76f264635fac28057888e5aba7bd5e499f825e1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:26:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = '홈 보고서 드라이버는 홈 환경을 위해 설계되었습니다.';
+$lang['home_reports_app_name'] = '홈 보고서';
+$lang['home_reports_banner'] = '홈 보고서에 오신 것을 환영합니다! 이 보고서는 가정 환경에서 유용하도록 설계되어 있습니다.';
+$lang['home_reports_home_reports'] = '홈 보고서';
