eef03ead68d31ab78af4aeb8c90f6a8cf72df527
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:31:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Драйвер Головна Звіти призначений для домашнього середовища.';
+$lang['home_reports_app_name'] = 'Головна Звіти';
+$lang['home_reports_banner'] = 'Ласкаво просимо на сайт Звіти! Ці звіти призначені, щоб бути корисним в домашніх умовах.';
+$lang['home_reports_home_reports'] = 'Головна Звіти';
