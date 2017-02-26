2c60166cf023f15c34ec3bae7cf2f5cf1a354215
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:29:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Vodič Home Reports je určený pre domáce prostredie.';
+$lang['home_reports_app_name'] = 'domáce Správy';
+$lang['home_reports_banner'] = 'Vitajte na domovskej správach! Tieto správy sú navrhnuté tak, aby boli užitočné v domácom prostredí.';
+$lang['home_reports_home_reports'] = 'domáce Správy';
