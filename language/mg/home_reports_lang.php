a4783caa8d9d373936ff6785a3731dcdf52c3cf7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:26:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'The Home Reports mpamily Natao ho an-trano tontolon&#39;ny.';
+$lang['home_reports_app_name'] = 'Home Reports';
+$lang['home_reports_banner'] = 'Tongasoa eto amin&#39;ny Home Reports! Ireo tatitra Natao ho ilaina ao an-trano tontolon&#39;ny.';
+$lang['home_reports_home_reports'] = 'Home Reports';
