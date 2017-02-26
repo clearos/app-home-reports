8b8c196fcf77915d6d60be827917e72b590f0dea
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:29:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/es/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'El conductor Inicio Eventos está diseñado para entornos domésticos.';
+$lang['home_reports_app_name'] = 'Informes Inicio';
+$lang['home_reports_banner'] = 'Bienvenido a los Informes de Inicio! Estos informes están diseñados para ser útil en entornos domésticos.';
+$lang['home_reports_home_reports'] = 'Informes Inicio';
