fea2d7b76a867d4ed8def89b0695ce1416fd7d1b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:24:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'મુખ્ય પૃષ્ઠ અહેવાલો ડ્રાઈવર ઘર પર્યાવરણો માટે રચાયેલ છે.';
+$lang['home_reports_app_name'] = 'મુખ્ય પૃષ્ઠ અહેવાલો';
+$lang['home_reports_banner'] = 'મુખ્ય પૃષ્ઠ અહેવાલો માટે આપનું સ્વાગત છે! આ અહેવાલો ઘર પર્યાવરણોમાં ઉપયોગી હોઈ માટે રચાયેલ છે.';
+$lang['home_reports_home_reports'] = 'મુખ્ય પૃષ્ઠ અહેવાલો';
