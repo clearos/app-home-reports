f8e9cda5d8220c209a3700c6be36da3c33b3d11a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:29:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Mokhanni Home Reports e etselitsoeng dibaka lapeng.';
+$lang['home_reports_app_name'] = 'Home Reports';
+$lang['home_reports_banner'] = 'Welcome ho Reports Home! litlaleho tse tsena li etselitsoe ho re tsoela molemo ka dibaka lapeng.';
+$lang['home_reports_home_reports'] = 'Home Reports';
