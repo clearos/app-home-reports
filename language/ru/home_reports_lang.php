6e4ce9d3332472877911142b60f7502ce1389475
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:28:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Драйвер Главная Отчеты предназначен для домашней среды.';
+$lang['home_reports_app_name'] = 'Главная Отчеты';
+$lang['home_reports_banner'] = 'Добро пожаловать на сайт Отчеты! Эти отчеты предназначены, чтобы быть полезным в домашних условиях.';
+$lang['home_reports_home_reports'] = 'Главная Отчеты';
