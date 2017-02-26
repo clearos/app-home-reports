2107b5ee5fa8447176d4086a04ce403e235be5e3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:25:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'The Home Skýrslur bílstjóri er hannað fyrir umhverfi heima.';
+$lang['home_reports_app_name'] = 'Skýrslur';
+$lang['home_reports_banner'] = 'Velkomin á heimasíðu Skýrslur! Þessar skýrslur eru hönnuð til að vera gagnleg í umhverfi heima.';
+$lang['home_reports_home_reports'] = 'Skýrslur';
