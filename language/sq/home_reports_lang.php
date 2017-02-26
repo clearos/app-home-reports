de6b62ce7679a144cdd40d2964f2009fbc508810
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:21:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Shoferi Kreu Raportet është projektuar për mjedise në shtëpi.';
+$lang['home_reports_app_name'] = 'Faqja Raportet';
+$lang['home_reports_banner'] = 'Mirë se vini në Raportet shtëpi! Këto raporte janë të dizajnuara të jenë të dobishme në mjediset shtëpiake.';
+$lang['home_reports_home_reports'] = 'Faqja Raportet';
