626a0bb9e3db3b207eaa3069e6944907a3519f28
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:26:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'The Home Reports auriga est ad domum environments.';
+$lang['home_reports_app_name'] = 'domus Reports';
+$lang['home_reports_banner'] = 'Welcome to Home Reports! Ambitus domum nuntiis utilitate ordinantur.';
+$lang['home_reports_home_reports'] = 'domus Reports';
