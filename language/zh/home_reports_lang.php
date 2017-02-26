379e9c0bb6256d44d98609ebaa73b4a59e43c0a0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:22:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = '主页报告驱动程序是专为家庭环境。';
+$lang['home_reports_app_name'] = '主页最新报道';
+$lang['home_reports_banner'] = '欢迎来到主页最新报道！ 这些报告被设计为在家庭环境中很有用。';
+$lang['home_reports_home_reports'] = '主页最新报道';
