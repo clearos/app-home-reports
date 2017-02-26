6a3b7ca0dd09431a52afc218b98f75cd21739b24
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:30:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'dereva Nyumbani Ripoti imeundwa kwa ajili ya mazingira ya nyumbani.';
+$lang['home_reports_app_name'] = 'nyumbani Ripoti';
+$lang['home_reports_banner'] = 'Karibu Ripoti Nyumbani! Taarifa hizi zimeundwa ili kuwa na manufaa katika mazingira ya nyumbani.';
+$lang['home_reports_home_reports'] = 'nyumbani Ripoti';
