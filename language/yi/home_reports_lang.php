f9634847530d2c43cc640731977811f0c7bd80d6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:31:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'די היים רעפּאָרץ שאָפער איז דיזיינד פֿאַר היים ינווייראַנמאַנץ.';
+$lang['home_reports_app_name'] = 'היים רעפּאָרץ';
+$lang['home_reports_banner'] = 'ברוכים הבאים צו דער היים רעפּאָרץ! די מעלדעט זענען דיזיינד צו זיין נוצלעך אין שטוב ינווייראַנמאַנץ.';
+$lang['home_reports_home_reports'] = 'היים רעפּאָרץ';
