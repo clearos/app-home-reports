d0a8ad976a9806a18696985a5ad7a7e17953e49a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:21:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Hasiera Txostenak gidariari etxeetarako diseinatuta dago.';
+$lang['home_reports_app_name'] = 'Hasiera-txostenak';
+$lang['home_reports_banner'] = 'Ongi Home Txostenak etorri! Txosten horiek diseinatuta daude etxeko inguruneetan erabilgarria izan.';
+$lang['home_reports_home_reports'] = 'Hasiera-txostenak';
