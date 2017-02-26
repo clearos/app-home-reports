edb3014ce3faf249a2be48e7f4600fe7ccecd63e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:27:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Нүүр хуудас тайлан драйвер гэрийн орчинд зориулагдсан.';
+$lang['home_reports_app_name'] = 'Нүүр хуудас тайлан';
+$lang['home_reports_banner'] = 'Нүүр хуудас тайлан тавтай морилно уу! Эдгээр тайлан гэрийн орчинд ашигтай байх зорилготой юм.';
+$lang['home_reports_home_reports'] = 'Нүүр хуудас тайлан';
