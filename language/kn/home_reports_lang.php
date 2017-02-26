fec9f9b5ed47af3b6d39c9eb5ebab6d3c3559242
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:25:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'ಮುಖಪುಟ ವರದಿಗಳು ಚಾಲಕ ಮನೆಯ ಪರಿಸರದಲ್ಲಿ ವಿನ್ಯಾಸಗೊಳಿಸಲಾಗಿದೆ.';
+$lang['home_reports_app_name'] = 'ಮುಖಪುಟ ವರದಿಗಳು';
+$lang['home_reports_banner'] = 'ಮುಖಪುಟ ವರದಿಗಳು ಸ್ವಾಗತ! ಈ ವರದಿಗಳು ಮನೆಯ ಪರಿಸರದಲ್ಲಿ ಉಪಯುಕ್ತ ವಿನ್ಯಾಸಗೊಳಿಸಲಾಗಿದೆ.';
+$lang['home_reports_home_reports'] = 'ಮುಖಪುಟ ವರದಿಗಳು';
