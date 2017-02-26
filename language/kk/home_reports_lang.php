08ce7fee6308111d451cddb67f48ee596d6e2af7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:25:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Басты драйвері үй ортада арналған есептер.';
+$lang['home_reports_app_name'] = 'Басты бет Есептер';
+$lang['home_reports_banner'] = 'Басты бет Баяндамалар қош келдіңіздер! Бұл есептер үй ортада пайдалы болуы үшін әзірленген.';
+$lang['home_reports_home_reports'] = 'Басты бет Есептер';
