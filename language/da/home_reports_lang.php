35756527624e891b9fd7083eaeb0222fbd122985
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:22:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'The Home Reports driveren er designet til private miljøer.';
+$lang['home_reports_app_name'] = 'Hjem Rapporter';
+$lang['home_reports_banner'] = 'Velkommen til Home rapporter! Disse rapporter er designet til at være anvendelige i hjemlige omgivelser.';
+$lang['home_reports_home_reports'] = 'Hjem Rapporter';
