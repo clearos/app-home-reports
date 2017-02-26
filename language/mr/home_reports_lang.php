b8b1d420309353ab2af7839193148c648cc533b7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:27:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'मुख्यपृष्ठ अहवाल ड्राइव्हर घरी वातावरणात डिझाइन केलेले आहे.';
+$lang['home_reports_app_name'] = 'मुख्यपृष्ठ अहवाल';
+$lang['home_reports_banner'] = 'मुख्यपृष्ठ अहवाल आपले स्वागत आहे! हे अहवाल घरी वातावरणात उपयुक्त असल्याचे डिझाइन केले आहेत.';
+$lang['home_reports_home_reports'] = 'मुख्यपृष्ठ अहवाल';
