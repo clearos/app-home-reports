e9fc4c9a96e2fa92b3a4e6d7b1447e3bea702a8d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:26:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'The Home Reports draiveris ir paredzēta mājas vidē.';
+$lang['home_reports_app_name'] = 'Sākums Ziņojumi';
+$lang['home_reports_banner'] = 'Laipni lūdzam Home Reports! Šie ziņojumi ir paredzēti, lai būtu noderīga mājas vidē.';
+$lang['home_reports_home_reports'] = 'Sākums Ziņojumi';
