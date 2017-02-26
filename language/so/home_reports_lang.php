7651bb7d7bbb5bd5c7924c308d52d779a3cfeba2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:29:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'The darawalka Home Wararka waxaa loogu talagalay deegaanno guriga.';
+$lang['home_reports_app_name'] = 'Wararka Home';
+$lang['home_reports_banner'] = 'Ku soo dhawow Home Wararka ka! wararka waxaa loogu talagalay in ay waxtar leh in deegaanno guriga.';
+$lang['home_reports_home_reports'] = 'Wararka Home';
