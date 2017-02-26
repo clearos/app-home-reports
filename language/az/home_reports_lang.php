405fff0919675b48be90e6cc705a49ccac326513
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:21:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Ana Hesabatlar sürücü ev mühit üçün nəzərdə tutulmuşdur.';
+$lang['home_reports_app_name'] = 'Baş səhifə Hesabatlar';
+$lang['home_reports_banner'] = 'Ana Hesabatlar xoş gəlmisiniz! Bu hesabatlar ev mühitində faydalı olmaq üçün nəzərdə tutulmuşdur.';
+$lang['home_reports_home_reports'] = 'Baş səhifə Hesabatlar';
