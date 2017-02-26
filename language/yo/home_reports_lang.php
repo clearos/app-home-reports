3ec2c08f0e9ca1821a90b7bf0706b06e0e6c7b24
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:32:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'The Home Iroyin iwakọ ti a ṣe fun ile ayika.';
+$lang['home_reports_app_name'] = 'Home Iroyin';
+$lang['home_reports_banner'] = 'Kaabo si Home Iroyin! Awọn wọnyi iroyin ti wa ni a še lati wa ni wulo ninu ile ayika.';
+$lang['home_reports_home_reports'] = 'Home Iroyin';
