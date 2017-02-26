98ea5fa169c9b43bb8e6ad319e0e69621cfb15cf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:25:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'ホームレポートドライバは、家庭環境のために設計されています。';
+$lang['home_reports_app_name'] = 'ホームレポート';
+$lang['home_reports_banner'] = 'ホームレポートへようこそ！ これらのレポートは、家庭環境において有用であるように設計されています。';
+$lang['home_reports_home_reports'] = 'ホームレポート';
