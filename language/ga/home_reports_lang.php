f0a91288463376a32d2b2aa236295b53757b70a7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:24:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Is é an tiománaí Baile Tuairiscí atá deartha le haghaidh timpeallachtaí bhaile.';
+$lang['home_reports_app_name'] = 'Tuarascálacha Baile';
+$lang['home_reports_banner'] = 'Fáilte chuig Tuarascálacha Baile! Tá na tuarascálacha gur ceapadh iad chun a bheith úsáideach i dtimpeallachtaí bhaile.';
+$lang['home_reports_home_reports'] = 'Tuarascálacha Baile';
