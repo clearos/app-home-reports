19e39d2428ede6ff70e73e953cbf40309690ad6b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:24:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Az otthon jelentések meghajtó az otthoni környezetben.';
+$lang['home_reports_app_name'] = 'Főoldal jelentések';
+$lang['home_reports_banner'] = 'Üdvözöljük a jelentések! Ezek a jelentések, hogy hasznos lehet az otthoni környezetben.';
+$lang['home_reports_home_reports'] = 'Főoldal jelentések';
