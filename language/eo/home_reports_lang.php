f5c50d39ca397b47be7121e7fd24ff989ec524c2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:23:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'La Hejmo Raportoj ŝoforo estas desegnita por hejma medioj.';
+$lang['home_reports_app_name'] = 'hejmo Raportoj';
+$lang['home_reports_banner'] = 'Bonvenon al la Hejmo Raportoj! Tiuj raportoj estas dizajnitaj por esti utila en hejma medio.';
+$lang['home_reports_home_reports'] = 'hejmo Raportoj';
