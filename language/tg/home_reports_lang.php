361fbfd22296b2cc72a4897a72fb64ef49aca15a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:30:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Ронанда Home Ҳисоботҳо аст, ки барои муҳити хона тарҳрезӣ.';
+$lang['home_reports_app_name'] = 'Ҳисобот Home';
+$lang['home_reports_banner'] = 'ба Ҳисобот Home Хуш омадед! Ин ҳисоботҳо тарҳрезӣ шудаанд, ки дар муҳити хона муфид бошад.';
+$lang['home_reports_home_reports'] = 'Ҳисобот Home';
