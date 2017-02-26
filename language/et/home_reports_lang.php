8ffb57bfeaa901fd83fc68c2b5f28bf0e9562de0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:23:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Kodu Reports juht on mõeldud kodukeskkonna.';
+$lang['home_reports_app_name'] = 'Kodu aruanded';
+$lang['home_reports_banner'] = 'Tere tulemast Kodu Reports! Need aruanded on mõeldud kasulik kodukeskkonna.';
+$lang['home_reports_home_reports'] = 'Kodu aruanded';
