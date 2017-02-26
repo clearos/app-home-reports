c6787082dce600467ede604eba65eb05052ead22
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:24:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'घर रिपोर्ट्स ड्राइवर घर के वातावरण के लिए बनाया गया है।';
+$lang['home_reports_app_name'] = 'घर रिपोर्ट्स';
+$lang['home_reports_banner'] = 'घर रिपोर्ट्स के लिए आपका स्वागत है! इन रिपोर्टों में घर के वातावरण में उपयोगी होने के लिए तैयार कर रहे हैं।';
+$lang['home_reports_home_reports'] = 'घर रिपोर्ट्स';
