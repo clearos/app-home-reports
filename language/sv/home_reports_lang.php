3bb3a4b0d0059156e101980ddff49d165ba9f15c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:30:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Home Rapporter drivrutinen är avsedd för hemmiljöer.';
+$lang['home_reports_app_name'] = 'Hem Rapporter';
+$lang['home_reports_banner'] = 'Välkommen till Hem Rapporter! Dessa rapporter är utformade för att vara användbara i hemmiljö.';
+$lang['home_reports_home_reports'] = 'Hem Rapporter';
