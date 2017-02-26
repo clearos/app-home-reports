1cc39ec3cfef867e716fa50a8f611d6bbee38593
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:24:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Sopir Depan Laporan dirancang untuk lingkungan rumah.';
+$lang['home_reports_app_name'] = 'rumah Laporan';
+$lang['home_reports_banner'] = 'Selamat datang di Laporan Depan! Laporan ini dirancang untuk menjadi berguna dalam lingkungan rumah.';
+$lang['home_reports_home_reports'] = 'rumah Laporan';
