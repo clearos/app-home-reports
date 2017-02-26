5c808370d5e35b56ac3c1b7f11a01d140bbba153
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:28:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'راننده خانه گزارشها برای محیط خانه طراحی شده است.';
+$lang['home_reports_app_name'] = 'گزارش اصلی';
+$lang['home_reports_banner'] = 'به گزارش خانه خوش آمدید! این گزارش ها طراحی شده اند تا در محیط های خانگی مفید باشد.';
+$lang['home_reports_home_reports'] = 'گزارش اصلی';
