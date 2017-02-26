240b4cc3b530a3f9af67278d17d03fd064a0add3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:22:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Woyendetsa Home Malipoti lakonzedwa mapangidwe kunyumba.';
+$lang['home_reports_app_name'] = 'Home Malipoti';
+$lang['home_reports_banner'] = 'Mwalandiridwa Malipoti Home! Malipoti amenewa apangidwa kukhala zothandiza mapangidwe kunyumba.';
+$lang['home_reports_home_reports'] = 'Home Malipoti';
