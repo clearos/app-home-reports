8420db5792ff78db047f74bc8e15af4c2b33640d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:31:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Bosh sahifa hisobotlar driver uy muhitlar uchun mo&#39;ljallangan.';
+$lang['home_reports_app_name'] = 'Bosh sahifa hisobotlar';
+$lang['home_reports_banner'] = 'Bosh sahifa hisobotlar xush kelibsiz! Bu hisobotlar uy muhitda foydali bo&#39;lishi uchun mo&#39;ljallangan.';
+$lang['home_reports_home_reports'] = 'Bosh sahifa hisobotlar';
