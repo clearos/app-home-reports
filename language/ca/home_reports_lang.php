659175960dd9d36ce814ae4657d7d76c138fd340
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:22:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'El conductor Esdeveniments està dissenyat per a entorns domèstics.';
+$lang['home_reports_app_name'] = 'informes Inici';
+$lang['home_reports_banner'] = 'Benvingut als Informes d&#39;Inici! Aquests informes estan dissenyats per ser útil en entorns domèstics.';
+$lang['home_reports_home_reports'] = 'informes Inici';
