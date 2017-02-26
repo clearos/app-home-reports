6a421759bef6a1eacb7dac9c719c36acefba8e58
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:27:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'गृह रिपोर्टहरू चालक घर वातावरण लागि डिजाइन गरिएको छ।';
+$lang['home_reports_app_name'] = 'घर प्रतिवेदन';
+$lang['home_reports_banner'] = 'गृह रिपोर्टहरू स्वागत छ! यी रिपोर्टहरू घर वातावरणमा उपयोगी हुन डिजाइन।';
+$lang['home_reports_home_reports'] = 'घर प्रतिवेदन';
