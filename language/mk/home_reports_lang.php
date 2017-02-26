04e64bea54b9c35ce0ed79fd98f757bf79cbfffd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:27:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Возачот на Почетна извештаи е наменета за домашни средини.';
+$lang['home_reports_app_name'] = 'Почетна Извештаи';
+$lang['home_reports_banner'] = 'Добредојдовте на Почетна Извештаи! Овие извештаи се дизајнирани да бидат корисни во домот средини.';
+$lang['home_reports_home_reports'] = 'Почетна Извештаи';
