cec0486754b4d554a30b0d117271c218f3887a28
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:21:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'تم تصميم برنامج التشغيل الرئيسية التقارير عن البيئات المنزلية.';
+$lang['home_reports_app_name'] = 'تقارير الرئيسية';
+$lang['home_reports_banner'] = 'مرحبا بكم في تقارير الرئيسية! وتهدف هذه التقارير إلى أن تكون مفيدة في البيئات المنزلية.';
+$lang['home_reports_home_reports'] = 'تقارير الرئيسية';
