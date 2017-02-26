c9156462d74bb99d50d7b07ba50b747cac5b174c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:31:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Các trình điều khiển Trang chủ Báo cáo được thiết kế cho môi trường gia đình.';
+$lang['home_reports_app_name'] = 'Trang chủ Báo cáo';
+$lang['home_reports_banner'] = 'Chào mừng bạn đến với Báo cáo Home! Các báo cáo này được thiết kế để thể hữu ích trong môi trường gia đình.';
+$lang['home_reports_home_reports'] = 'Trang chủ Báo cáo';
