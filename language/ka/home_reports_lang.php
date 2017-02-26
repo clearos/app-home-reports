00411731d768a3aa4fd2676f1a006aedca1915ad
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:25:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'მთავარი რეპორტაჟი მძღოლი განკუთვნილია სახლის გარემოში.';
+$lang['home_reports_app_name'] = 'მთავარი';
+$lang['home_reports_banner'] = 'მოგესალმებით მთავარი! ეს მოხსენებები მიზანია იყოს სასარგებლო სახლში გარემოში.';
+$lang['home_reports_home_reports'] = 'მთავარი';
