401837d0080d951f2e799a867661e2609aabe6e6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:23:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Home Raportit ohjain on suunniteltu kotiympäristöön.';
+$lang['home_reports_app_name'] = 'Home Raportit';
+$lang['home_reports_banner'] = 'Tervetuloa Home Raportit! Nämä raportit on suunniteltu hyödyllinen kotiympäristöihin.';
+$lang['home_reports_home_reports'] = 'Home Raportit';
