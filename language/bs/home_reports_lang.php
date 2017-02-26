bc268ede4ef4fcf06a3562ac4a6a77711c871fb0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:21:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Vozač Naslovna Izvještaji je dizajniran za kućno okruženje.';
+$lang['home_reports_app_name'] = 'Naslovna Izvještaji';
+$lang['home_reports_banner'] = 'Dobro došli na početni izvještaji! Ovi izvještaji su dizajnirane da budu korisni u kućnom okruženju.';
+$lang['home_reports_home_reports'] = 'Naslovna Izvještaji';
