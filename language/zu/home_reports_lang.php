af7462900782dca647c859943223a626842f5b76
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:32:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Lo mshayeli Home Imibiko yenzelwe ekhaya izindawo.';
+$lang['home_reports_app_name'] = 'Imibiko Home';
+$lang['home_reports_banner'] = 'Siyakwamukela Imibiko Ekhaya! Le mibiko eklanyelwe wusizo ekhaya ezindaweni.';
+$lang['home_reports_home_reports'] = 'Imibiko Home';
