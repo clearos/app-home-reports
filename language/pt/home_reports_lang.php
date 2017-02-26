f6b22d3c15dcf364c121060a7360ca8f6264326c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:28:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pt/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'O motorista Inicio Relatos é projetado para ambientes domésticos.';
+$lang['home_reports_app_name'] = 'Relatórios Início';
+$lang['home_reports_banner'] = 'Bem-vindo ao Reports Home! Estes relatórios são projetados para ser útil em ambientes domésticos.';
+$lang['home_reports_home_reports'] = 'Relatórios Início';
