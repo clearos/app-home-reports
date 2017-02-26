167fdc0532490a1bc80814598e691e029bcc9604
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:23:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/de/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Die Home-Reports-Treiber ist für den Heimumgebungen konzipiert.';
+$lang['home_reports_app_name'] = 'Home Meldungen';
+$lang['home_reports_banner'] = 'Willkommen auf der Startseite Berichte! Diese Berichte sind so konzipiert, in der häuslichen Umgebungen nützlich sein.';
+$lang['home_reports_home_reports'] = 'Home Meldungen';
