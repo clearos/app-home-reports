e3510f12e5cd7fbaf50ba01f4e04c58cedd1f2c8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:27:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Is-sewwieq Rapporti Dar hija mfassla għal ambjenti dar.';
+$lang['home_reports_app_name'] = 'Rapporti Home';
+$lang['home_reports_banner'] = 'Merħba lill-Home Rapporti! Dawn ir-rapporti huma mfassla sabiex ikunu utli f&#39;ambjenti dar.';
+$lang['home_reports_home_reports'] = 'Rapporti Home';
