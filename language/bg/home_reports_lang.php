096d6c535ec1facdc8cbdb8ee2dd0bea9a873668
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:22:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bg/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Шофьорът на Home Reports е предназначена за домашна среда.';
+$lang['home_reports_app_name'] = 'Начало Сигнали';
+$lang['home_reports_banner'] = 'Добре дошли в дома доклади! Тези доклади са предназначени да бъдат полезни при домашни условия.';
+$lang['home_reports_home_reports'] = 'Начало Сигнали';
