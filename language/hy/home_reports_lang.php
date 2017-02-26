919cc7752fbb4b4774373a051aba21b3677f2277
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:21:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'The Home Ռեպորտաժ վարորդը, որը նախատեսված է տնային միջավայրում:';
+$lang['home_reports_app_name'] = 'Գլխավոր էջ';
+$lang['home_reports_banner'] = 'Բարի գալուստ տուն հաշվետվությունների! Այդ հաշվետվությունները նախագծված են, որպեսզի օգտակար լինել տնային միջավայրում:';
+$lang['home_reports_home_reports'] = 'Գլխավոր էջ';
