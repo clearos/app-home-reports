9e8dd0debca56e69727cf3c8f22bfec6ca596939
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:23:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fr/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Le pilote Accueil Rapports est conçu pour les environnements domestiques.';
+$lang['home_reports_app_name'] = 'Accueil Rapports';
+$lang['home_reports_banner'] = 'Bienvenue aux rapports Accueil! Ces rapports sont conçus pour être utile dans les environnements domestiques.';
+$lang['home_reports_home_reports'] = 'Accueil Rapports';
