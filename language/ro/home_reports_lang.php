4f942359a7aa0dd487d00dff7f31c6257b3ad9ca
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:28:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Conducătorul auto Acasă Rapoartele este conceput pentru mediile casnice.';
+$lang['home_reports_app_name'] = 'Rapoarte Prima pagină';
+$lang['home_reports_banner'] = 'Bun venit în Home Rapoarte! Aceste rapoarte sunt proiectate pentru a fi utile în mediile casnice.';
+$lang['home_reports_home_reports'] = 'Rapoarte Prima pagină';
