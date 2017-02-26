4ab94bc76f80b99c8f8cca1127fb9188c376b6dd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:26:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Kei te hangaia te taraiwa Home Pūrongo mō ngā taiao kāinga.';
+$lang['home_reports_app_name'] = 'Kāinga pūrongo';
+$lang['home_reports_banner'] = 'Nau mai ki te Pūrongo Kāinga! E hangaia ēnei pūrongo ki te kia whai hua i roto i ngā taiao kāinga.';
+$lang['home_reports_home_reports'] = 'Kāinga pūrongo';
