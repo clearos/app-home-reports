1e46da656826bd4d9d30bb17fba047df9db7a99e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:20:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Die Home Verslae bestuurder is ontwerp vir die huis omgewings.';
+$lang['home_reports_app_name'] = 'Tuis Verslae';
+$lang['home_reports_banner'] = 'Welkom by die Huis Verslae! Hierdie verslae is ontwerp nuttig in die huis omgewings te wees.';
+$lang['home_reports_home_reports'] = 'Tuis Verslae';
