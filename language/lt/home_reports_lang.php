d2d53ddf91d517f40bdeaf09385486a24a8735c6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:26:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Home Ataskaitos vairuotojas yra skirtas namų aplinkoje.';
+$lang['home_reports_app_name'] = 'Pradžia Ataskaitos';
+$lang['home_reports_banner'] = 'Sveiki atvykę į pagrindinį ataskaitų! Šie pranešimai yra skirti būti naudinga namų aplinkoje.';
+$lang['home_reports_home_reports'] = 'Pradžia Ataskaitos';
