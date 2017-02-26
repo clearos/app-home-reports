d00ca2d9a081cf9f112194825d44446d95e8af87
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:27:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Pemandu Home Laporan direka untuk persekitaran rumah.';
+$lang['home_reports_app_name'] = 'Laporan rumah';
+$lang['home_reports_banner'] = 'Selamat datang ke Laporan Home! Laporan-laporan ini direka untuk menjadi berguna dalam persekitaran rumah.';
+$lang['home_reports_home_reports'] = 'Laporan rumah';
