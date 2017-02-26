fc7b3f92bc73dc0eaebdc31a7c20676fab0fcb2d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:29:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Supir Home Laporan dirancang pikeun lingkungan imah.';
+$lang['home_reports_app_name'] = 'Home Laporan';
+$lang['home_reports_banner'] = 'Wilujeng sumping ka Laporan Utama! laporan ieu dirarancang pikeun jadi mangpaat dina lingkungan imah.';
+$lang['home_reports_home_reports'] = 'Home Laporan';
