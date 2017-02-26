b4af0c81ed626dc5679810c23749cfdf88907add
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:24:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'The Home Akụkọ ọkwọ ụgbọala e mere maka ụlọ gburugburu.';
+$lang['home_reports_app_name'] = 'Home Reports';
+$lang['home_reports_banner'] = 'Mmadụ Bịa ka Home Akụkọ! Ndị a na-akọ na-iji ike bara uru na home gburugburu.';
+$lang['home_reports_home_reports'] = 'Home Reports';
