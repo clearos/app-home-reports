6f1f51d71861d74376207e02de56f1e42296037a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:24:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Se chofè a Rapò Kay ki fèt pou anviwònman lakay yo.';
+$lang['home_reports_app_name'] = 'Rapò Kay';
+$lang['home_reports_banner'] = 'Byenveni nan Rapò a Kay! Rapò sa yo yo fèt yo dwe itil nan yon anviwonman kay la.';
+$lang['home_reports_home_reports'] = 'Rapò Kay';
