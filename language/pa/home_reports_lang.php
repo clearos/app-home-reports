2096cfa4eef6d5f6a8ef0e0e940ea0c0ab3d7b24
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:28:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'ਮੁੱਖ ਰਿਪੋਰਟ ਡਰਾਈਵਰ ਨੂੰ ਘਰ ਵਾਤਾਵਰਣ ਲਈ ਤਿਆਰ ਕੀਤਾ ਗਿਆ ਹੈ.';
+$lang['home_reports_app_name'] = 'ਮੁੱਖ ਰਿਪੋਰਟ';
+$lang['home_reports_banner'] = 'ਮੁੱਖ ਰਿਪੋਰਟ ਦਾ ਸੁਆਗਤ ਹੈ! ਇਹ ਰਿਪੋਰਟ ਨੂੰ ਘਰ ਵਾਤਾਵਰਣ ਵਿੱਚ ਲਾਭਦਾਇਕ ਹੋਣ ਲਈ ਤਿਆਰ ਕੀਤੇ ਗਏ ਹਨ.';
+$lang['home_reports_home_reports'] = 'ਮੁੱਖ ਰਿਪੋਰਟ';
