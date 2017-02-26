78660194ad1e3828f783c7565e65535e001c5572
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:30:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Ang Home Reports driver ay dinisenyo para sa mga kapaligiran sa bahay.';
+$lang['home_reports_app_name'] = 'Reports Home';
+$lang['home_reports_banner'] = 'Maligayang pagdating sa Home Reports! Ang mga ulat na ito ay dinisenyo upang maging kapaki-pakinabang sa mga kapaligiran sa bahay.';
+$lang['home_reports_home_reports'] = 'Reports Home';
