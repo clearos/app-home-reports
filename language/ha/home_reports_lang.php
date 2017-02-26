f70794d525b0022e26f5e6946aa4fd0b2d3f5a87
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:24:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'The Home Rahotanni direba aka tsara don home halin wurare.';
+$lang['home_reports_app_name'] = 'Home Rahotanni';
+$lang['home_reports_banner'] = 'Barka da zuwa Home Rahotanni! Wadannan rahotanni an tsara su zama da amfani a gida halin wurare.';
+$lang['home_reports_home_reports'] = 'Home Rahotanni';
