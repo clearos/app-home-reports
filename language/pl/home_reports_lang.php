70d5c220c24fa1a5858cb77d4c3dd3e75a7043f5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:28:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Kierowca główna Raporty jest przeznaczony dla środowisk domowych.';
+$lang['home_reports_app_name'] = 'Home Raporty';
+$lang['home_reports_banner'] = 'Witamy na raporty! Raporty te są zaprojektowane tak, aby być przydatna w warunkach domowych.';
+$lang['home_reports_home_reports'] = 'Home Raporty';
