5b14066e7aafaa77e8a956657e3d3949ba9f16d3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:31:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Mae&#39;r gyrrwr Adref Adroddiadau wedi&#39;i gynllunio ar gyfer amgylcheddau cartref.';
+$lang['home_reports_app_name'] = 'Adroddiadau cartref';
+$lang['home_reports_banner'] = 'Croeso i&#39;r Adref Adroddiadau! yr adroddiadau hyn yn cael eu cynllunio i fod yn ddefnyddiol mewn amgylcheddau cartref.';
+$lang['home_reports_home_reports'] = 'Adroddiadau cartref';
