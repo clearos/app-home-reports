9560fd526b0a9e328e4edcfac883aab236c6b34c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:29:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'Gonilnik Home poročila je namenjen domačem okolju.';
+$lang['home_reports_app_name'] = 'Domov Poročila';
+$lang['home_reports_banner'] = 'Dobrodošli na domači poročil! Ta poročila so namenjeni za uporabno v domačem okolju.';
+$lang['home_reports_home_reports'] = 'Domov Poročila';
