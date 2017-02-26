eb93e33ff882b50a1ad6204f468a313219c95f8d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:27:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/home_reports_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['home_reports_app_description'] = 'De Home Rapporten driver is ontworpen voor thuis omgevingen.';
+$lang['home_reports_app_name'] = 'Home Rapporten';
+$lang['home_reports_banner'] = 'Welkom op de Home Rapporten! Deze rapporten zijn ontworpen nuttig in huis omgevingen te zijn.';
+$lang['home_reports_home_reports'] = 'Home Rapporten';
